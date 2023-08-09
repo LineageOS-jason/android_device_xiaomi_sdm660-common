c82596bd3ebae520e029df411226641035403576
Nanhumly <admin@warbler.cc>
2023-08-09 08:44:38 +0000

    Revert "sdm660-common: Don't explicitly build android.hidl.allocator@1.0"
    This reverts commit 2d09338cd3523ef767754285e386913783d681e8.
    
    Change-Id: I0b13676c59526c88f1b621eeebfadb40529fbd5f
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- sdm660.mk
+++ sdm660.mk
@@ -227,6 +227,7 @@ endif
 
 # HIDL
 PRODUCT_PACKAGES += \
+    android.hidl.allocator@1.0.vendor \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
     android.hidl.memory@1.0.vendor \
