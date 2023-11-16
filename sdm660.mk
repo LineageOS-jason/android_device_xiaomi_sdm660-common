c71885c434d73a743a9f24b2b566af59edd573c2
Nanhumly <admin@warbler.cc>
2023-11-16 05:42:01 +0000

    fixup! sdm660-common: Remove vendor RenderScript implementation
    Change-Id: I15c204ed1bfb6f6777f6b82bf300110d9562b78e
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- sdm660.mk
+++ sdm660.mk
@@ -348,10 +348,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libjson
 
-# RenderScript HAL
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4.vendor \
