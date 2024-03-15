a6f0ac414edab944d5c65400495776bdaa5b93bf
Yumi Yukimura <me.cafebabe@gmail.com>
2024-03-15 15:17:25 +0800

    Revert "sdm660-common: Switch to legacy WiFi HIDL HAL"
    * WiFi AIDL HAL works in U QPR2
      (also, the legacy WiFi HIDL HAL has stopped working, lol)
    
    This reverts commit 9ad8e602f74bb16212e1b5282de4a34c842dbd90.
    
    Change-Id: I4d38fcc7e5999f934344bc364afb4f4d8a049711

--- sdm660.mk
+++ sdm660.mk
@@ -398,7 +398,7 @@ PRODUCT_COPY_FILES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service.legacy \
+    android.hardware.wifi-service \
     hostapd \
     libwifi-hal-qcom \
     libwpa_client \
