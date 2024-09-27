d18801a55967bce86846ffaa87b87cde9b470853
Nanhumly <admin@warbler.cc>
2024-09-27 14:13:13 +0000

    sdm660-common: Drop BOARD_VNDK_VERSION
    * Deprecated in A15.
    
      https://android.googlesource.com/platform/build/+/66481ae8e1525325792030862a16a9befafd58bb
    
    Change-Id: I1d095208d00bbb830d544d15b29e8c376c46ece4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -127,7 +127,6 @@ PRODUCT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
-BOARD_VNDK_VERSION := current
 
 # Verity
 # Only needed for signing
