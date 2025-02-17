6145758594f948dc5daf11c58bac3831cb51e385
Flicker372 <flicker372@outlook.com>
2025-02-17 15:42:23 +0800

    sdm660-common: Import task_profiles_30.json from android-15.0.0_r17
    Change-Id: I456ad023840ad81b9ff0b4a170915b6d6fc6c0c7

--- sdm660.mk
+++ sdm660.mk
@@ -137,7 +137,7 @@ endif
 # Control groups and task profiles
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
