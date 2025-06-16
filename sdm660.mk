9eb5861c47e53e6f0ee8cd0682ddb2f55b66280c
basamaryan <basam.aryan@gmail.com>
2025-06-16 11:19:48 -0400

    sdm660-common: Drop legacy ANT remnants
    QSSI only has platform/external/ant-wireless/hidl since a while
    
    Change-Id: Ibccfdc7e72efd3c9d3ff62bda3e729b99433a567

--- sdm660.mk
+++ sdm660.mk
@@ -103,10 +103,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0 \
