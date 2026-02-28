awana111/device_asus_sdm660-common

sdm660-common: Import task_profiles_30.json from android-15.0.0_r17
Change-Id: I456ad023840ad81b9ff0b4a170915b6d6fc6c0c7
Signed-off-by: texascake <tiktodz@soulvibe-team.cloud>

sdm660.mk
@@ -114,7 +114,7 @@ PRODUCT_PACKAGES += \
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    $(COMMON_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # Charger
 PRODUCT_PRODUCT_PROPERTIES += \

LineageOS-jason/android_device_xiaomi_sdm660-common

sdm660.mk
@@ -141,7 +141,7 @@ endif
 # Control groups and task profiles
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true